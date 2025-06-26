9b4b15ee521efa59e947db2290cdc828d037e563
bharathkumaracharicaerulean <bharathkumaracharips@gmail.com>
2025-06-26 14:18:53 +0530

    added navbar component to front end

---
+++ src/coders_playground_web3_frontend/src/Dashboard/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Demo } from '../components/Navbar-comp';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Demo />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/coders_playground_web3_frontend/src/components/Navbar-comp.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../ui/Navbar"
+
+const Demo = () => {
+    return (
+        <>
+            <Navbar/>
+        </>
+    )
+}
+
+export {Demo}
